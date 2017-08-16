e6bcbda42d77857ea20b85b35453027130378ddc
Nicholas Gervais <gerva038@d.umn.edu>
2017-08-15 23:27:03 -0500

    added two js files, one for normal jquery and then the other for minified jquery

---
+++ scripts/jquery.js
@@ -0,0 +1,124 @@
+// scroll to different sections
+
+$(".home-link").click(function(){
+    $('html,body').animate({
+    scrollTop: $('body').offset().top},
+    'slow');
+});
+
+$(".about-link").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".about").offset().top},
+        'slow'); 
+    closeSideNav();
+});
+
+
+$(".projects-link").click(function(){
+    $('html,body').animate({
+    scrollTop: $('.projects').offset().top},
+    'slow');
+    closeSideNav();
+});
+
+$(".hobbies-link").click(function(){
+    $('html,body').animate({
+    scrollTop: $('.hobbies').offset().top},
+    'slow');
+    closeSideNav();
+});
+
+$(".resume-link").click(function(){
+    $('html,body').animate({
+    scrollTop: $('.resume').offset().top},
+    'slow');
+    closeSideNav();
+});
+
+
+
+$(".contact-link").click(function(){
+    $('html,body').animate({
+    scrollTop: $('.contact').offset().top},
+    'slow');
+    closeSideNav();
+});
+
+//calls setHeaderAndNavHeight on every window resize event
+$(window).resize(function(){
+    setHeaderAndNavHeight();
+});
+
+// calls windowBelowContent on every scroll event
+$(window).scroll(function() {
+    windowBelowContent();
+    scrollReadMore();
+});
+// calls windowBelowContent and setHeaderAndNavHeight when page is ready
+$(document).ready(function() {
+    windowBelowContent();
+    scrollReadMore();
+    setHeaderAndNavHeight();
+});
+
+// toggle right nav open/closed
+$("#nav-icon").click(function(e){
+    e.stopPropagation();
+    $("#right-nav").toggleClass("open");
+    $("#nav-icon").toggleClass("open");
+});
+
+$("#right-nav, navbar").click(function(e){
+    e.stopPropagation();
+});
+
+$("html,body").click(function(){
+    closeSideNav();
+});
+
+function closeSideNav() {
+    $('#right-nav').removeClass('open');
+    $("#nav-icon").removeClass("open");
+}
+
+// Submit contact form
+$("#message-form").submit(function(event){
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    var url = "https://hooks.slack.com/services/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"
+    var payload={"channel": "#portfolio-page", "username": email , "text": name + ': ' + message, "icon_emoji": ":ghost:"};
+    $.post(url,JSON.stringify(payload), function(data){
+    console.log(data);
+    $("#message-form").trigger("reset");
+    });
+    event.preventDefault();
+});
+
+
+// checks if the top of the window is below the content and changes classes accordingly
+function windowBelowContent(){
+    var top = $(".about-cta").offset().top;
+    if ($(this).scrollTop() >= top - 5) {
+    $("#navbar").addClass("black-nav");
+    } else {
+    $("#navbar").removeClass("black-nav");
+    }
+}
+
+function scrollReadMore(){
+    if($(this).scrollTop() >= 20) {
+    $(".read-more").hide();
+    } else {
+    $(".read-more").show();
+    }
+}
+
+function setHeaderAndNavHeight(){
+    $(".header").css({
+    height: $(window).height()
+    });
+    $("#right-nav").css({
+    height: $(".header").height()
+    });
+}
